9dca4fc8a023df45033d082b23aeeed1f5c077e0
JasonHears <jearshear@gmail.com>
2016-11-30 22:43:33 -0800

    clean up of codebase: remove node_modules; add new packages; add api framework; add test framework;

---
+++ test/apiRoot.test.ts
@@ -0,0 +1,26 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import app from '../src/App';
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('baseRoute', () => {
+
+  it('should be json', () => {
+    chai.request(app).get('/')
+    .then(res => {
+      expect(res.type).to.eql('application/json');
+    });
+  });
+
+  it('should have a message prop', () => {
+    chai.request(app).get('/')
+    .then(res => {
+      expect(res.body.message).to.eql('Action Now API v1.0');
+    });
+  });
+
+});
