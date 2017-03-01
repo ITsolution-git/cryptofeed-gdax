4fa63a326ce3913d137b7a234c4367fd89785d84
Roy Smith <roy.smith0820@gmail.com>
2017-03-01 03:39:16 -0500

    delete duplicates

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -3,6 +3,8 @@ import {IRequest} from '../classes/IRequest';
 const validate = require('../classes/ParamValidator');
 import AuthValidation from '../validations/AuthValidation';
 
+var request = require('request');
+
 const tokenHelpers = require('../tools/tokens');
 const toolHelpers = require('../tools/_helpers');
 var util = require('util');
@@ -91,6 +93,70 @@ export class AuthRouter {
     });
   }
 
+  public loginFacebook(req: IRequest, res: Response, next: NextFunction) {
+  //   var fields = ['id', 'email', 'first_name', 'last_name', 'link', 'name'];
+  //   var accessTokenUrl = 'https://graph.facebook.com/v2.5/oauth/access_token';
+  //   var graphApiUrl = 'https://graph.facebook.com/v2.5/me?fields=' + fields.join(',');
+  //   var params = {
+  //     code: req.body.code,
+  //     client_id: req.body.clientId,
+  //     client_secret: config.FACEBOOK_SECRET,
+  //     redirect_uri: req.body.redirectUri
+  //   };
+
+  //   // Step 1. Exchange authorization code for access token.
+  //   request.get({ url: accessTokenUrl, qs: params, json: true }, function(err, response, accessToken) {
+  //     if (response.statusCode !== 200) {
+  //       return res.status(500).send({ message: accessToken.error.message });
+  //     }
+
+  //     // Step 2. Retrieve profile information about the current user.
+  //     request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
+  //       if (response.statusCode !== 200) {
+  //         return res.status(500).send({ message: profile.error.message });
+  //       }
+  //       if (req.header('Authorization')) {
+  //         User.findOne({ facebook: profile.id }, function(err, existingUser) {
+  //           if (existingUser) {
+  //             return res.status(409).send({ message: 'There is already a Facebook account that belongs to you' });
+  //           }
+  //           var token = req.header('Authorization').split(' ')[1];
+  //           var payload = jwt.decode(token, config.TOKEN_SECRET);
+  //           User.findById(payload.sub, function(err, user) {
+  //             if (!user) {
+  //               return res.status(400).send({ message: 'User not found' });
+  //             }
+  //             user.facebook = profile.id;
+  //             user.picture = user.picture || 'https://graph.facebook.com/v2.3/' + profile.id + '/picture?type=large';
+  //             user.displayName = user.displayName || profile.name;
+  //             user.save(function() {
+  //               var token = createJWT(user);
+  //               res.send({ token: token });
+  //             });
+  //           });
+  //         });
+  //       } else {
+  //         // Step 3. Create a new user account or return an existing one.
+  //         User.findOne({ facebook: profile.id }, function(err, existingUser) {
+  //           if (existingUser) {
+  //             var token = createJWT(existingUser);
+  //             return res.send({ token: token });
+  //           }
+  //           var user = new User();
+  //           user.facebook = profile.id;
+  //           user.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
+  //           user.displayName = profile.name;
+  //           user.save(function() {
+  //             var token = createJWT(user);
+  //             res.send({ token: token });
+  //           });
+  //         });
+  //       }
+  //     });
+  //   });
+  // });
+
+  }
   /**
    * Take each handler, and attach to one of the Express.Router's
    * endpoints.
