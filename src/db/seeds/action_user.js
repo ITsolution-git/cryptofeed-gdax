62a81be17853cac1daf24a2f909276323e566b64
Roy Smith <rsmith@nodejs-512mb-sfo1-01-actodo.localdomain>
2017-03-02 04:04:13 -0800

    seed data change

--- src/db/seeds/action_user.js
+++ src/db/seeds/action_user.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('action_user').insert({action_user_id:1, action_id: 1, user_id: 1, points: 5}),
+        //knex('action_user').insert({action_user_id:1, action_id: 1, user_id: 1, points: 5}),
         knex('action_user').insert({action_user_id:2, action_id: 2, user_id: 1, points: 5, skip:true})
       ]);
     });
